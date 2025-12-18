d30843ec6d7b261ae953be71bf166b79b6519cb9
SaiVineeth <saivineeth07@gmail.com>
2025-12-18 01:56:07 -0500

    Editing Threshold File

--- app/Imagethresholding.py
+++ app/Imagethresholding.py
@@ -4,7 +4,7 @@ import os
 
 def main():
     # Read image 
-    src = cv2.imread(r"C:\Users\saivi\Downloads\Numbers.jpg", cv2.IMREAD_GRAYSCALE) 
+    src = cv2.imread(r"images\Numbers.jpg", cv2.IMREAD_GRAYSCALE) 
     
     # Basic threshold example 
     th, dst = cv2.threshold(src, 0, 255, cv2.THRESH_BINARY); 
