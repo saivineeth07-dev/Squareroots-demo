4c402746ea3a19fda9f379b0de675cb891fdc9f8
SaiVineeth <saivineeth07@gmail.com>
2026-01-02 15:31:32 +0530

    Editing Files

--- app/Annotation.py
+++ app/Annotation.py
@@ -4,23 +4,19 @@ import os
 def main():
     img1 = cv2.imread(r"C:\Users\saivi\Downloads\Tiger.jpg")
     img2 = cv2.imread(r"C:\Users\saivi\Downloads\Car.jpg")
+
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
+    workspace = os.getenv("WORKSPACE", os.getcwd())
+    output_dir = os.path.join(workspace, "output")
 
-    if os.getenv("CI") == "true":
-        # CI mode → save output instead of showing
-        cv2.imwrite(r"C:\Users\saivi\OneDrive\Pictures\Screenshots\output_gray1.jpg", gray1)
-        cv2.imwrite(r"C:\Users\saivi\OneDrive\Pictures\Screenshots\output_gray2.jpg", gray2)
+    os.makedirs(output_dir, exist_ok=True)
 
-    else:
-        # Local mode → show window
-        cv2.imshow("grayscale image1", gray1)
-        cv2.imshow("grayscale image2", gray2)
-        cv2.imwrite(r"C:\Users\saivi\OneDrive\Pictures\Screenshots\dog_gray1.jpg", gray1)
-        cv2.imwrite(r"C:\Users\saivi\OneDrive\Pictures\Screenshots\dog_gray2.jpg", gray2)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    cv2.imwrite(os.path.join(output_dir, "output_gray1.jpg"), gray1)
+    cv2.imwrite(os.path.join(output_dir, "output_gray2.jpg"), gray2)
+
+    print(f"Images saved in: {output_dir}")
 
 if __name__ == "__main__":
     main()
