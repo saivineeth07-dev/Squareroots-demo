9cba8b2222831c75410cc04278052a9c68eaa135
SaiVineeth <saivineeth07@gmail.com>
2026-01-02 14:08:28 +0530

    Editing Files

--- app/Annotation.py
+++ app/Annotation.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 def main():
-    img = cv2.imread(r"C:\Users\saivi\Downloads\Car.jpg")
+    img = cv2.imread(r"C:\Users\saivi\Downloads\Tiger.jpg")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     if os.getenv("CI") == "true":
